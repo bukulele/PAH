6e7a7cd6b3b8ed724097843881baa22a2266cd16
nukulele <zonough@gmail.com>
2022-12-01 18:38:44 +0400

    small style updates

--- js/script.js
+++ js/script.js
@@ -12,8 +12,6 @@ let chatData = {
   windowWidth: null,
 
   init: function () {
-    // console.log(document.documentElement.clientHeight);
-    console.log($(document).get());
     chatData.windowWidth = $(window).get(0).innerWidth;
     $(window).resize(chatData.handleWindowWidth);
     chatData.loadUserData();
