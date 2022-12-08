b6aa303097c78a9ab189fef4da821a1a6c463c24
nukulele <zonough@gmail.com>
2022-12-08 15:11:40 +0400

    sending images option disabled

--- js/script.js
+++ js/script.js
@@ -273,17 +273,31 @@ let chatData = {
       (!$("#sendMessageButton").get(0).style.display ||
         $("#sendMessageButton").get(0).style.display === "none")
     ) {
-      $("#sendPhotoIcon").fadeOut(100, function () {
-        $("#sendMessageButton").fadeIn(150);
-      });
+      $("#sendMessageButton").fadeIn(150);
     } else if (
       !target.value.trim().length &&
       $("#sendMessageButton").get(0).style.display
     ) {
-      $("#sendMessageButton").fadeOut(100, function () {
-        $("#sendPhotoIcon").fadeIn(150);
-      });
+      $("#sendMessageButton").fadeOut(100);
     }
+
+    // THIS IS THE VERSION OF SWITCHING BETWEEN PHOTO OR MESSAGE SEND BUTTONS
+    // if (
+    //   target.value.trim().length &&
+    //   (!$("#sendMessageButton").get(0).style.display ||
+    //     $("#sendMessageButton").get(0).style.display === "none")
+    // ) {
+    //   $("#sendPhotoIcon").fadeOut(100, function () {
+    //     $("#sendMessageButton").fadeIn(150);
+    //   });
+    // } else if (
+    //   !target.value.trim().length &&
+    //   $("#sendMessageButton").get(0).style.display
+    // ) {
+    //   $("#sendMessageButton").fadeOut(100, function () {
+    //     $("#sendPhotoIcon").fadeIn(150);
+    //   });
+    // }
   },
 
   defineDateFormat: function (date) {
@@ -395,14 +409,13 @@ let chatData = {
   },
 
   hideEmojiBlock: function (e) {
-    console.log(e);
     if (e.target.id === "emojiBlockWrapper") {
       $(".message-window__emoji-block-wrapper").fadeOut();
     }
   },
 
   sendMessage: function () {
-    const dateNow = new Date();
+    // const dateNow = new Date();
     // chatData.conversations[chatData.selectedChat].messages.push({
     //   date: dateNow,
     //   message: $("#newMessageInput").val(),
@@ -630,22 +643,6 @@ let chatData = {
           >
           Send
           </button>
-          <svg
-          id="sendPhotoIcon"
-          class="new-message__send-photo-icon"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 512 512"
-        >
-          <!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
-          <path
-            d="M152 120c-26.51 0-48 21.49-48 48s21.49 48 48 48s48-21.49 48-48S178.5 120 152 120zM447.1 32h-384C28.65 32-.0091 60.65-.0091 96v320c0 35.35 28.65 64 63.1 64h384c35.35 0 64-28.65 64-64V96C511.1 60.65 483.3 32 447.1 32zM463.1 409.3l-136.8-185.9C323.8 218.8 318.1 216 312 216c-6.113 0-11.82 2.768-15.21 7.379l-106.6 144.1l-37.09-46.1c-3.441-4.279-8.934-6.809-14.77-6.809c-5.842 0-11.33 2.529-14.78 6.809l-75.52 93.81c0-.0293 0 .0293 0 0L47.99 96c0-8.822 7.178-16 16-16h384c8.822 0 16 7.178 16 16V409.3z"
-          />
-        </svg>
-        <input
-          type="file"
-          id="addPicture"
-          accept="image/*"
-          style="display:none" />
           </div>
           `);
       } else if (
